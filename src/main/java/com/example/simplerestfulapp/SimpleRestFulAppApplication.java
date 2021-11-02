2b4cd64ce6ea7998799c5517379f14a2be6baebd
Konstantin Boryagin <nokss89@gmail.com>
2021-11-02 20:20:21 +0300

    initialize

---
+++ src/main/java/com/example/simplerestfulapp/SimpleRestFulAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplerestfulapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestFulAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRestFulAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplerestfulapp/SimpleRestFulAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplerestfulapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestFulAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
